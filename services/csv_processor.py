eb296f3f4031a9fec5f6aa5a1275fa75e23b781d
qdorleat <58438730+qdorleat@users.noreply.github.com>
2026-05-26 09:49:17 +0200

    Use the date from csv file

--- services/csv_processor.py
+++ services/csv_processor.py
@@ -1,9 +1,7 @@
-from datetime import datetime
-
 import pandas as pd
 import streamlit as st
 
-def process_csv(uploaded_file):
+def process_csv(uploaded_file, date_match):
     # 1. SAFELY LOCATE THE HEADER ROW INDEX DYNAMICALLY
     uploaded_file.seek(0)
     header_row_index = None
@@ -55,13 +53,11 @@ def process_csv(uploaded_file):
     # 2. Remove row if it's a duplicate (keeps the first occurrence) (filter by firstname, name and date of birth)
     df = df.drop_duplicates(subset=[df.columns[1], df.columns[2], df.columns[3]], keep="first")
 
-    # Get today's date formatted as DD/MM/YYYY
-    today_str = datetime.now().strftime("%d/%m/%Y")
+    # 3. Keep only rows where 'Arrivée' matches today's date
+    year, month, day = date_match.groups()
+    df_filtered = df[df["Arrivée"] == f"{day}/{month}/{year}" ]
 
-    # 3. Keep only rows where 'Arriv�e' matches today's date
-    df_filtered = df[df["Arrivée"] == today_str]
-
-    # 3. Rename the first column and overwrite all its row values
+    # 4. Rename the first column and overwrite all its row values
     df_filtered = df_filtered.rename(columns={df_filtered.columns[0]: "Etablissement"})
     df_filtered["Etablissement"] = "La Maladaire"
 
