e5b5f857b58b7af2d43be2302c85e9d786ddccd5
qdorleat <58438730+qdorleat@users.noreply.github.com>
2026-06-20 12:13:52 +0200

    Update excel_exporter.py
    Tentative d'exporter le excel avec des points.

--- services/excel_exporter.py
+++ services/excel_exporter.py
@@ -1,18 +1,41 @@
 import pandas as pd
 
+
 def export_to_excel(df, output_path):
     with pd.ExcelWriter(output_path, engine="xlsxwriter") as writer:
+
+        sheet_name = output_path.stem[:31]  # limite Excel
+
         df.to_excel(
             writer,
-            sheet_name=output_path.name,
+            sheet_name=sheet_name,
             index=False
         )
 
-        worksheet = writer.sheets[output_path.name]
+        workbook = writer.book
+        worksheet = writer.sheets[sheet_name]
+
+        date_format = workbook.add_format({
+            "num_format": "dd.mm.yyyy"
+        })
 
         for idx, col in enumerate(df.columns):
             max_len = max(
                 df[col].astype(str).map(len).max(),
                 len(col)
             )
-            worksheet.set_column(idx, idx, max_len + 2)
+
+            # Works only with datetime format
+            if pd.api.types.is_datetime64_any_dtype(df[col]):
+                worksheet.set_column(
+                    idx,
+                    idx,
+                    max(max_len + 2, 12),
+                    date_format
+                )
+            else:
+                worksheet.set_column(
+                    idx,
+                    idx,
+                    max_len + 2
+                )
